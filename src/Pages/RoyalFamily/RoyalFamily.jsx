82a232a1b1b9dce6206e112062a62596c293a85f
Ilan Gustav Veras <ilanbarrosveras3@gmail.com>
2024-03-05 11:23:12 -0400

    RoyalFamily completa

--- src/Pages/RoyalFamily/RoyalFamily.jsx
+++ src/Pages/RoyalFamily/RoyalFamily.jsx
@@ -4,40 +4,95 @@ import ArrayPeople from '../../Components/ArrayPeople/ArrayPeople'
 import ArrayPeopleRoyalty from '../../Components/ArrayPeopleRoyalty/ArrayPeopleRoyalty'
 import peopleOfNorta from "../../assets/peopleOfNorta"
 import "./RoyalFamily.css"
-console.log(peopleOfNorta)
+
+
 export default function RoyalFamily(){
+  //definido um array apenas de prateados
+  const silvers = peopleOfNorta.silverHouses
 
-        const silvers = peopleOfNorta.silverHouses
+  //convertendo o objeto 'peopleOfNorta' em um array para podermos trabalhar com as funções filter e map
+  function peopleOfNortaArray(objParametro){
+    return Object.values(objParametro)
+  }
 
-        function peopleOfNortaArray(objeto) {
-          return Object.values(objeto);
-        }
-        
-        // console.log(peopleOfNortaArray(silvers));
-        
-        const arraySilvers = []
-        const arrayRoyalty = []
-        const [personName,setPersonName] = useState("Maven Merandus Calore")
-        const [housePerson,setHousePerson] = useState("Calore")
-        
-        //families
-        const calore = peopleOfNorta.silverHouses.calore
-        const samos = peopleOfNorta.silverHouses.samos
-        const merandus = peopleOfNorta.silverHouses.merandus
-        console.log(merandus)
-        function generateRoyalPeople(array){
-          const royalty = array.people.filter(person => person.royal)
-          array.peopleRoyalty = royalty
-          return array
-        }
+  //array que filtra da propriedade "people" apenas os que te a propeiedade 'royal' == true
+  function genereteRoyalPeople(array){
+    const royalty = array.people.filter(person => person.royal)
+    return {...array, peopleRoyalty: royalty}
+  }
 
-        //colocar apenas os da realeza em um array específico
+  //função que verifica em cada casa a existência de alguém em peopleRoyalty
+  function generateRoyalPeopleForHouses(houses){
+    return houses.map(house => genereteRoyalPeople(house))
+  }
 
-        return (
-            <div className='second-container-ArrayPeople'>
-              <ArrayPeopleRoyalty array={generateRoyalPeople(calore)}></ArrayPeopleRoyalty>
-              <ArrayPeopleRoyalty array={generateRoyalPeople(merandus)}></ArrayPeopleRoyalty>
-            </div>
-        )
+  const silverHousesArray = peopleOfNortaArray(silvers)
+  const silverHousesWithRoyalty = generateRoyalPeopleForHouses(silverHousesArray)
+
+  return(
+    <div className='second-container-ArrayPeople'>
+      {silverHousesWithRoyalty.map((house, index) => (
+        <ArrayPeopleRoyalty key={index} array={house}></ArrayPeopleRoyalty>
+      ))}
+    </div>
+  )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export default function RoyalFamily(){
+
+//         const silvers = peopleOfNorta.silverHouses
+
+//         function peopleOfNortaArray(objeto) {
+//           return Object.values(objeto);
+//         }
+        
+        
+//         const arraySilvers = []
+//         const arrayRoyalty = []
+//         const [personName,setPersonName] = useState("Maven Merandus Calore")
+//         const [housePerson,setHousePerson] = useState("Calore")
+        
+//         //families
+//         const calore = peopleOfNorta.silverHouses.calore
+//         const samos = peopleOfNorta.silverHouses.samos
+//         const merandus = peopleOfNorta.silverHouses.merandus
+//         function generateRoyalPeople(array){
+//           const royalty = array.people.filter(person => person.royal)
+//           array.peopleRoyalty = royalty
+//           arrayRoyalty.push(array)
+//           return array
+//         }
+
+//         function t(a){
+//           a.map((i, key) => {
+//             // console.log(i.people)
+//             return generateRoyalPeople(i)
+//           })
+//         }
+
+//         const y = t(peopleOfNortaArray(silvers))
+//         console.log(y)
+
+        
+//         //colocar apenas os da realeza em um array específico
+
+//         return (
+//             <div className='second-container-ArrayPeople'>
+//               <ArrayPeopleRoyalty array={generateRoyalPeople(calore)}></ArrayPeopleRoyalty>
+//               <ArrayPeopleRoyalty array={generateRoyalPeople(merandus)}></ArrayPeopleRoyalty>
+//             </div>
+//         )
+// }
 
