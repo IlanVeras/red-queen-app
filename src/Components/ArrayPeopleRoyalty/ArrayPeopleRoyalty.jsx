4c4428ecdcd743d7c78d3253767c0bc9f8288f6a
Ilan Gustav Veras <ilanbarrosveras3@gmail.com>
2024-03-05 11:39:29 -0400

    estilizar para que os cards fiquem um ao lado do outro

--- src/Components/ArrayPeopleRoyalty/ArrayPeopleRoyalty.jsx
+++ src/Components/ArrayPeopleRoyalty/ArrayPeopleRoyalty.jsx
@@ -3,7 +3,7 @@ import "./ArrayPeopleRoyalty.css"
 
 export default function ArrayPeopleRoyalty(props){
     return(
-        <div className="container-ArrayPeople">
+        <div className="container-ArrayPeopleRoyalty">
             {
                 props.array.peopleRoyalty.map((person,key) => (
                     <Card key={key.toString()} personName={person.name} houseName={props.array.houseName} houseColors={props.array.houseColors}></Card>
